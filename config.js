b4aa9eb528797c4eb1e36a2a8c58efe6fb3adba1
Dylan Sheffer <dsheffer16@gmail.com>
2017-07-12 22:22:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ module.exports = {
   // }
   target: {
     name: "Status_Lunch",
-    url: "https://slack.com/api/users.profile.set?token=xoxp-72229371188-72520190322-212130773778-df0dc78acb485e0c3f6ed19eb2678bef&profile=%7B%0D%0A++++%22status_text%22%3A+%22Lunch%22%2C%0D%0A++++%22status_emoji%22%3A+%22%3Afork_and_knife%3A%22%0D%0A%7D"
+    url: "https://slack.com/api/users.profile.set?token={secret token here}&profile=%7B%0D%0A++++%22status_text%22%3A+%22Lunch%22%2C%0D%0A++++%22status_emoji%22%3A+%22%3Afork_and_knife%3A%22%0D%0A%7D"
   }
 };
