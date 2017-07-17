1e4f3bc7e46cf039ebc53b742bfec26df4510014
Dylan Sheffer <dsheffer16@Dylans-MacBook-Pro.local>
2017-07-17 10:52:13 -0400

    Dash button will toggle my slack status

--- config.js
+++ config.js
@@ -1,13 +1,29 @@
 module.exports = {
-  dash: {
-    MAC_address: "fc:a6:67:4e:73:57"
-  },
-  // target: {
-  //   name: "IFTTT",
-  //   url: "https://maker.ifttt.com/trigger/dash_pressed/with/key/b1v0DKWan69uayGMBZh462"
-  // }
-  target: {
-    name: "Status_Lunch",
-    url: "https://slack.com/api/users.profile.set?token=xoxp-72229371188-72520190322-212130773778-df0dc78acb485e0c3f6ed19eb2678bef&profile=%7B%0D%0A++++%22status_text%22%3A+%22Lunch%22%2C%0D%0A++++%22status_emoji%22%3A+%22%3Afork_and_knife%3A%22%0D%0A%7D"
-  }
+    dash: {
+        MAC_address: "68:37:e9:d3:a2:71"
+    },
+    getProfile: {
+        name: "Get Profile",
+        requestOptions: {
+            host: "slack.com",
+            path: "/api/users.profile.get?token=xoxp-72229371188-72520190322-213112437684-ee90cf3cecbc9f1708751320a7c95fbd",
+            method: "GET"
+        }
+    },
+    statusLunch: {
+        name: "Status Lunch",
+        requestOptions: {
+            host: "slack.com",
+            path: "/api/users.profile.set?token=xoxp-72229371188-72520190322-213112437684-ee90cf3cecbc9f1708751320a7c95fbd&profile=%7B%0D%0A++++%22status_text%22%3A+%22Lunch%22%2C%0D%0A++++%22status_emoji%22%3A+%22%3Afork_and_knife%3A%22%0D%0A%7D",
+            method: "POST"
+        }
+    },
+    resetStatus: {
+        name: "Reset Status",
+        requestOptions: {
+            host: "slack.com",
+            path: "/api/users.profile.set?token=xoxp-72229371188-72520190322-213112437684-ee90cf3cecbc9f1708751320a7c95fbd&profile=%7B%0A%20%20%20%20%22status_text%22%3A%20%22%22%2C%0A%20%20%20%20%22status_emoji%22%3A%20%22%22%0A%7D",
+            method: "POST"
+        }
+    }
 };
--- dash.js
+++ dash.js
@@ -1,20 +1,67 @@
 var config = require('./config');
 var dash_button = require('node-dash-button');
-var dash = dash_button(config.dash.MAC_address, null, null, 'all');
+var Promise = require('promise');
+var dash = dash_button(config.dash.MAC_address, 'en2', null, 'all');
 
-dash.on("detected", function (){
+dash.on("detected", function() {
     console.log("dash pressed");
     toggleStatus();
 });
 
-var toggleStatus = function() {
-  var https = require('https');
-  console.log('Calling ' + config.target.name);
-  console.log('URL: ' + config.target.url);
+function request(target) {
+    var https = require('https');
+    console.log('Calling ' + target.name);
+    var requestOptions = target.requestOptions;
+    var body = '';
+    var head = '';
 
-  https.get(config.target.url)
-    .on("error", function(e){
-      console.log("Got error: " + e.message);
+    return new Promise((resolve, reject) => {
+        var response = https.request(requestOptions, (result) => {
+
+            result.setEncoding('utf8');
+
+            result.on('data', (chunk) => {
+                body += chunk;
+            });
+
+            result.on('error', (err) => {
+                console.log('Error encountered while making request.');
+                reject(err);
+            });
+
+            result.on('end', () => {
+
+                var output = {
+                    'head': result.headers,
+                    'body': JSON.parse(body)
+                };
+
+                resolve(output);
+            });
+        });
+        response.end();
     });
+}
+
+
+var toggleStatus = function() {
+    var currentState = request(config.getProfile);
+
+    currentState.done((value) => {
+        console.log("Status: " + JSON.stringify(value.body.profile.status_text));
+
+        if (value.body.profile.status_text === "") {
+            request(config.statusLunch);
+        } else {
+            request(config.resetStatus);
+        }
+        console.log("Finished Request");
+    });
+
+    currentState.catch((reason) => {
+        console.log(reason);
+        process.abort();
+    });
+
 
 }
